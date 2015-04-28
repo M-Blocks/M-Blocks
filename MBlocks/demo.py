e672e8dc87014fcff2bd4b66fe2cc3f5a1dfa0a1
Sebastian Claici <sebastianclaici@gmail.com>
2015-04-28 12:11:47 -0400

    Temporary demo function in case cubes stop working.

--- MBlocks/demo.py
+++ MBlocks/demo.py
@@ -9,48 +9,51 @@ do_change = True
 
 def change_color(cube):
     colors = list(chain(combinations('rgb', 1), combinations('rgb', 2)))
-    while True:
+    for i in range(60):
         for face in range(1, 7):
             color = ''.join(random.sample(colors, 1)[0])
             cube.ser.write(' '.join(('fbrgbled', color, 'tb', str(face))) + '\n')
         time.sleep(2)
 
-def ceo_demo():
+def stop_color(cube):
+    cube.ser.write('fbrgbled off tb 1 2 3 4 5 6\n')
+
+def ceo_demo_temp(ports):
+    cubes = [Cube(port) for port in ports]
+
+    time.sleep(5)
+    for i in range(3):
+        for j in range(2):
+            for cube in cubes:
+                cube.ser.write('ia f 6000 1800 20\n')
+            time.sleep(5)
+        for j in range(2):
+            for cube in cubes:
+                cube.ser.write('ia r 6000 1900 20\n')
+            time.sleep(5)
+
+    for cube in cubes:
+        cube.disconnect()
+
+def ceo_demo(ports):
     """
     The MAC addresses of the cubes are:
-       - 1: C9:6E:6B:9F:96:B6   (mostly red screws)
-       - 2: E2:8A:4C:9A:6F:9C   (mostly blue screws)
-       - 3: D8:9C:00:41:E4:19   (mostly black screws)
-
+       - 1: E2:8A:4C:9A:6F:9C   (mostly blue screws)
+       - 2: D8:9C:00:41:E4:19   (mostly black screws)
     """
-    ports = ['/dev/ttyACM' + str(x) for x in range(1,2)]
-    print ports
-
     cubes = [Cube(port) for port in ports]
     for cube in cubes:
-        if cube.mac_address == 'C9:6E:6B:9F:96:B6':
-            fst = cube
-            fst.disconnect()
-        elif cube.mac_address == 'E2:8A:4C:9A:6F:9C':
-            scd = cube
-        elif cube.mac_address == 'D8:9C:00:41:E4:19':
-            lst = cube
-            lst.disconnect()
+        thread.start_new_thread(change_color, (cube,))
 
-    thread.start_new_thread(change_color, (scd,))
-    # fst.ser.write('ia f 10000 3500 5\n')  # Corner climb
-    # time.sleep(3)
-    # fst.ser.write('ia f 5000 2500 10\n')  # Traverse
-    # time.sleep(3)
-
-    # scd.ser.write('ia f 13000 4000 4 e 15\n')  # Vertical climb
-    # time.sleep(3)
-    # scd.ser.write('ia f 13000 4000 4 e 15\n')  # Vertical convex
-    # time.sleep(3)
-
-    # Random motion
-    # scd.ser.write('ia f 6000 2500 10\n') # Traverse
-    # scd.change_plane('left')
+    for i in range(3):
+        for j in range(2):
+            for cube in cubes:
+                cube.ser.write('ia f 6000 1300 20\n')
+            time.sleep(3)
+        for j in range(2):
+            for cube in cubes:
+                cube.ser.write('ia f 6000 1500 20\n')
+            time.sleep(3)
 
 if __name__ == '__m in__':
     ceo_demo()

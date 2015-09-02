1aa7777922fd6851d9ecae1bbcd8eb88c70ccd84
Sebastian Claici <sebastianclaici@gmail.com>
2015-09-02 15:02:35 -0700

    Added function to connect on all available ports

--- MBlocks/discovery.py
+++ MBlocks/discovery.py
@@ -7,7 +7,7 @@ used = set()
 
 def discovery():
     while True:
-        ports = [port[0] for port in serial.tools.list_ports.comports() if 'ACM' in port[0]]
+        ports = [port[0] for port in serial.tools.list_ports.comports()]
         for port in ports:
             if port in used:
                 continue
@@ -20,6 +20,23 @@ def discovery():
                 print 'Port {0} is not available'.format(port)
             except NoCubeException:
                 print 'There is no cube on port {0}'.format(port)
+
+def connect_all():
+    """Connect to all ports and return a list of cubes on those ports.
+    """
+    ports = [port[0] for port in serial.tools.list_ports.comports()]
+    cubes = []
+    for port in ports:
+        try:
+            cube = Cube(port)
+            print 'Connected to cube {0}'.format(cube.mac_address)
+            cubes.append(cube)
+        except serial.SerialException:
+            print 'Port {0} is not available'.format(port)
+        except NoCubeException:
+            print 'There is no cube on port {0}'.format(port)
+
+    return cubes
 
 def acquire(port):
     """Acquire hold on port.
--- disconnect.py
+++
@@ -1,7 +0,0 @@
-import serial
-
-ports = input('Enter ports to disconnect (separated by spaces -- e.g. \'com6 com7 com8\'): ').split()
-for port in ports:
-    ser = serial.Serial(port, baudrate=115200)
-    ser.write('blediscon\n')
-    ser.close()

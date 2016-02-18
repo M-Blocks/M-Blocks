ae71478679f6f7fc46cb0d6df723c3cdff99a809
Sebastian Claici <sebastianclaici@gmail.com>
2016-02-18 10:21:43 -0500

    [ADD] Two cube light tracker. Pending testing.

--- MBlocks/control/twocube.py
+++ MBlocks/control/twocube.py
@@ -4,10 +4,9 @@ import operator
 
 
 class TwoCubeController(object):
-    central_label = 'Central Alignment'
-    
     def __init__(self, fst, scd):
-        if fst.config[central_label] != (0, 0, 1):
+        fst_d = tuple(fst.find_plane())
+        if fst_d != (0, 0, 1) and fst_d != (0, 0, -1):
             self.driver = fst
             self.steer = scd
         else:
@@ -16,18 +15,18 @@ class TwoCubeController(object):
 
         # ensure steer is oriented correctly
         self.steer.change_plane((0, 0, 1))
-        center_d = self.driver.config[central_label]
-        center_s = self.steer.config[central_label]
-        if np.all(np.absolute(center_d) == (0, 0, 1)):
-            # both cubes are oriented with MB parallel to ground
-            self.driver.change_plane((1, 1, 0))
-
-        # TEMP: use light sensor values to check if driver's forward/backward direction is towards steer
-        self.driver._find_config()
-        center_d = self.driver.config[central_label]
         
-        lights = self.driver._read_light_sensors()
-        if light_values['Forward'] == 0 or light_values['Backward'] == 0:
+        center_d = tuple(self.driver.find_plane())
+        if center_d == (0, 0, 1) or center_d == (0, 0, -1):
+            # both cubes are oriented with MB parallel to ground
+            self.driver.change_plane((1, -1, 0))
+
+        # use light sensor values to check if driver's forward/backward direction is towards steer
+        self.driver.find_config()
+        center_d = tuple(self.driver.find_plane())
+        
+        fc, rc = self.driver.config['forward'], self.driver.config['reverse']
+        if self.driver.is_connected(fc) or self.driver.is_connected(rc):
             if center_d == (1, 1, 0) or center_d == (-1, -1, 0):
                 self.driver.change_plane((1, -1, 0))
             else:
@@ -44,33 +43,35 @@ class TwoCubeController(object):
         """ 
         direction = None
         while direction != 'forward' and direction != 'reverse':
-            light_values = self.driver._read_light_sensors()
+            light_values = self.driver.read_light_sensors()
             sorted_values = sorted(light_values.items(), key=operator.itemgetter(1), reverse=True)
             for k, v in sorted_values:
-                if k == 'Top' or k == 'Bottom':
+                if k == 'top' or k == 'bottom':
                     continue
-                if k == 'Forward':
+                if k == 'forward':
                     direction = 'forward'
-                elif k == 'Backward':
+                elif k == 'reverse':
                     direction = 'reverse'
-                elif sorted_values['Forward'] > sorted_values['Backward']:
-                    self.steer(direction='f')
                 else:
-                    self.steer(direction='r')
+                    self.steer_cubes()
 
         # TODO: add check to see if both actions were successfully completed
-        self.leader.do_action('two_cube_traverse', '{}'.format(direction))
-        self.leader.do_action('two_cube_traverse', '{}'.format(direction))
+        self.driver.do_action('two_cube_traverse', '{}'.format(direction))
+        self.driver.do_action('two_cube_traverse', '{}'.format(direction))
 
         # We are OK if either both actions completed, or if neither did
-        self.driver._find_config()
-        center = self.driver.config[central_label]
-        while np.any(np.absolute(center) != (0, 0, 1)):
+        center = tuple(self.steer.find_plane())
+        while center != (0, 0, 1):
             # only one move successfully completed
-            self.leader.do_action('two_cube_traverse', '{}'.format(direction))
+            self.driver.do_action('two_cube_traverse', '{}'.format(direction))
 
-    def steer(self, direction):
-        self.leader.ser.write('bldcspeed {} 8000\n'.format(direction))
-        time.sleep(4)
-        self.leader.ser.write('bldcstop b\n')
+    def steer_cubes(self, sleep=4):
+        # steer towards the larger light value
+        light_values = self.steer.read_light_sensors()
+        if light_values['forward'] > light_values['reverse']:
+            self.steer.ser.write('bldcspeed f 8000\n')
+        else:
+            self.steer.ser.write('bldcspeed r 8000\n')
+        time.sleep(sleep)
+        self.steer.ser.write('bldcstop b\n')
 
